6b7e9e2ddea0d73969cb0a107dcce87956d70faa
RigolimHairandco <andercleidesigner@gmail.com>
2025-07-04 08:50:04 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App'; // Importa o nosso componente principal
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+// Renderiza o nosso componente principal
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
