a432d5eee8cb83330b87a631ee9bdfbed02427fb
RigolimHairandco <andercleidesigner@gmail.com>
2025-07-09 12:18:33 -0300

    Update dataHooks.js

--- src/hooks/dataHooks.js
+++ src/hooks/dataHooks.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'; 
 import { collection, query, orderBy, onSnapshot, where } from 'firebase/firestore';
 import { db } from '../firebase';
 
